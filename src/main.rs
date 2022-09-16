449f4775d6422ecdce21716d1813d5d74de47768
Steve Cook <steviecook210@gmail.com>
2022-09-16 09:02:54 -0400

    Use rustls

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::env;
+use std::fs::File;
+use std::io::BufReader;
 use actix_cors::Cors;
 use actix_web::{get, web, HttpServer, Responder, App, HttpResponse};
 use actix_web::http::header;
 use reqwest::Client;
+use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{certs, pkcs8_private_keys};
 use serde::{Deserialize, Serialize};
 use time::format_description::well_known;
 use time::{OffsetDateTime, UtcOffset};
@@ -141,6 +145,8 @@ async fn game_time(info: web::Query<TimeReq>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
+    let config = load_rustls_config();
+
     HttpServer::new(|| {
 
         let cors = Cors::default()
@@ -151,7 +157,40 @@ async fn main() -> std::io::Result<()> {
             .max_age(3600);
 
         App::new().wrap(cors).service(teams).service(game_time)
-    }).bind(("0.0.0.0", 8080))?
+    }).bind_rustls(("0.0.0.0", 8080), config)?
         .run()
         .await
+}
+
+fn load_rustls_config() -> ServerConfig {
+    // init server config builder with safe defaults
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth();
+
+    let path = env::var("PEM_DIR").unwrap();
+
+    // load TLS key/cert files
+    let cert_file = &mut BufReader::new(File::open(format!("{}/rootCA.pem", path)).unwrap());
+    let key_file = &mut BufReader::new(File::open(format!("{}/rootCA-key.pem", path)).unwrap());
+
+    // convert files to key/cert objects
+    let cert_chain = certs(cert_file)
+        .unwrap()
+        .into_iter()
+        .map(Certificate)
+        .collect();
+    let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file)
+        .unwrap()
+        .into_iter()
+        .map(PrivateKey)
+        .collect();
+
+    // exit if no keys could be parsed
+    if keys.is_empty() {
+        eprintln!("Could not locate PKCS 8 private keys.");
+        std::process::exit(1);
+    }
+
+    config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
 }
