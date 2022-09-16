80c248c3c40f9b69c1a66952f8ccb319127091e0
Steve Cook <steviecook210@gmail.com>
2022-09-16 02:11:03 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,146 @@
+use std::env;
+use actix_web::{get, web, HttpServer, Responder, App, HttpResponse};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use time::format_description::well_known;
+use time::{OffsetDateTime, UtcOffset};
+
+#[derive(Debug, Deserialize)]
+pub struct TimeReq {
+    year: u16,
+    week: u8,
+    team: String,
+    offset: i8,
+}
+
+#[derive(Debug, Serialize, Default)]
+pub struct TimeResp {
+    team: String,
+    start: String,
+    kickoff: String,
+    end: String,
+    start_trans: String,
+    kickoff_trans: String,
+    end_trans: String,
+}
+
+#[derive(Deserialize)]
+struct Game {
+    start_date: String,
+}
+
+#[derive(Deserialize)]
+struct Play {
+    wallclock: String,
+}
+
+#[get("/teams")]
+async fn teams() -> impl Responder {
+
+    let token = env::var("CFB_TOKEN").unwrap();
+
+    let client = Client::new();
+    let temp = client.get("https://api.collegefootballdata.com/teams/fbs?year=2022")
+        .bearer_auth(token)
+        .send().await;
+
+    if let Ok(resp) = temp {
+        HttpResponse::Ok().body(resp.text().await.unwrap())
+    } else {
+        HttpResponse::BadRequest().finish()
+    }
+}
+
+#[get("/time")]
+async fn game_time(info: web::Query<TimeReq>) -> impl Responder {
+
+    let token = env::var("CFB_TOKEN").unwrap();
+
+    let client = Client::new();
+    let temp = client.get(format!(
+        "https://api.collegefootballdata.com/games?year={}&week={}&seasonType=regular&team={}",
+        info.year, info.week, info.team
+    ))
+        .bearer_auth(token.clone())
+        .send().await;
+
+    let mut response = TimeResp::default();
+
+    if let Ok(resp) = temp {
+        let game = resp.json::<Vec<Game>>().await;
+        if let Ok(game) = game {
+            if !game.is_empty() {
+                let start_time =
+                    OffsetDateTime::parse(&game.first().unwrap().start_date, &well_known::Rfc3339)
+                        .expect("Failed to parse start date");
+                let start_time_trans =
+                    start_time.to_offset(UtcOffset::from_hms(info.offset, 0, 0).unwrap());
+
+                response.team = info.team.clone();
+                response.start = format!("{:0>2}:{:0>2}", start_time.hour(), start_time.minute());
+                response.start_trans = format!(
+                    "{:0>2}:{:0>2}",
+                    start_time_trans.hour(),
+                    start_time_trans.minute()
+                );
+
+                let temp = client.get(format!(
+                    "https://api.collegefootballdata.com/plays?seasonType=regular&year={}&week={}&team={}",
+                    info.year, info.week, info.team
+                ))
+                    .bearer_auth(token)
+                    .send().await;
+
+                if let Ok(resp) = temp {
+                    let plays = resp.json::<Vec<Play>>().await;
+                    if let Ok(plays) = plays {
+                        let first = plays.first().unwrap();
+                        let last = plays.last().unwrap();
+                        let kickoff_time =
+                            OffsetDateTime::parse(&first.wallclock, &well_known::Rfc3339)
+                                .expect("Failed to parse kickoff time");
+                        let kickoff_time_trans =
+                            kickoff_time.to_offset(UtcOffset::from_hms(info.offset, 0, 0).unwrap());
+
+                        response.kickoff =
+                            format!("{:0>2}:{:0>2}", kickoff_time.hour(), kickoff_time.minute());
+                        response.kickoff_trans = format!(
+                            "{:0>2}:{:0>2}",
+                            kickoff_time_trans.hour(),
+                            kickoff_time_trans.minute()
+                        );
+
+                        let end_time = OffsetDateTime::parse(&last.wallclock, &well_known::Rfc3339)
+                            .expect("Failed to parse end time");
+                        let end_time_trans =
+                            end_time.to_offset(UtcOffset::from_hms(info.offset, 0, 0).unwrap());
+
+                        response.end = format!("{:0>2}:{:0>2}", end_time.hour(), end_time.minute());
+                        response.end_trans = format!(
+                            "{:0>2}:{:0>2}",
+                            end_time_trans.hour(),
+                            end_time_trans.minute()
+                        );
+                    }
+                }
+
+                HttpResponse::Ok().body(serde_json::to_string(&response).unwrap())
+            } else {
+                HttpResponse::Ok().body("")
+            }
+        } else {
+            HttpResponse::Ok().body("")
+        }
+    } else {
+        HttpResponse::Ok().body("")
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(teams).service(game_time)
+    }).bind(("0.0.0.0", 8080))?
+        .run()
+        .await
+}
